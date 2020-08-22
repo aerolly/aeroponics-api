d832f3b329b785347e7cd58c0aef54cc0f64d381
Spencer Young <spjy@hawaii.edu>
2020-08-22 09:45:04 -1000

    feat: create parse number float

--- src/app.py
+++ src/app.py
@@ -16,12 +16,13 @@ import settings
 
 load_dotenv()
 
-
-
+# Connect to redis server
 r = redis.Redis(host=os.getenv('REDIS_IP'), port=os.getenv('REDIS_PORT'), db=0)
+
+# Create redis pubsub object
 p = r.pubsub(ignore_subscribe_messages=True)
 
-
+# Connect to postgres with driver
 conn = psycopg2.connect(
          user = os.environ.get('SQL_USER'),
          password = os.environ.get('SQL_PASS'),
@@ -29,15 +30,30 @@ conn = psycopg2.connect(
          port = os.environ.get('SQL_PORT'),
          database = os.environ.get('SQL_DB'))
 
-
+# Define flask server
 app = Flask(__name__)
+
+# Define rest api server
 api = Api(app)
+
+# Allow cross origin communication
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Define socket io server
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+# Server to client communication endpoint
 @socketio.on('')
 def send_data(data):
   socketio.emit('data', json.loads(data))
+
+
+# Utility function to parse number if possible
+def parseNumber(number):
+  try:
+    return float(value)
+  except ValueError as error:
+    return value
 
 ############################################################
 # DB Query Functions
@@ -63,6 +79,8 @@ def modifyDB(funcName, params):
 
 #############################################################
 
+# Endpoint definitions
+
 class Sensor(Resource):
   def get(self):
     return getResultSetFromDB('"Device".view_availablesensors' , [])
@@ -72,12 +90,14 @@ class Controller(Resource):
     return getResultSetFromDB('"Device".view_availablecontrollers' , [])
 
 class Command(Resource):
+  # Send a command to be scheduled
   def post(self):
     print(request.data)
     r.publish('scheduler', request.data)
     return 200
 
 class System(Resource):
+  # Get value from the current system state
   def get(self):
     initialState = {}
 
@@ -85,20 +105,16 @@ class System(Resource):
       # delete the key
       value = r.get(key).decode('utf-8')
 
-      try: 
-        initialState[key.decode('utf-8')] = int(value)
-      except ValueError:
-        initialState[key.decode('utf-8')] = value
+      initialState[key.decode('utf-8')] = parseNumber(value)
 
     return initialState
+
+  # Set a value for the system state
   def post(self):
     data = json.loads(request.data)
     r.set(data['key'], data['value'])
 
-    try: 
-      val = int(data['value'])
-    except ValueError:
-      val = data['value']
+    val = parseNumber(data['value'])
 
     send_data(json.dumps({
       'key': data['key'],
@@ -106,11 +122,13 @@ class System(Resource):
     }))
     return 200
 
+# Define URL extension for each endpoint definition
 api.add_resource(Sensor, '/sensor')
 api.add_resource(Controller, '/controller')
 api.add_resource(Command, '/command')
 api.add_resource(System, '/system')
 
+# Loop until redis data arrives
 def handleRedisData():
   try:
     p.subscribe('data')
