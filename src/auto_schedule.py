a66b9a9af92b27ee0f9720dcf087b29297af064b
Mastanese <kl26@hawaii.edu>
2020-10-19 01:02:41 -1000

    feat: safe exit

--- src/auto_schedule.py
+++ src/auto_schedule.py
@@ -15,80 +15,103 @@ p = r.pubsub(ignore_subscribe_messages=True)
 
 
 while True:
-    # Check if scheduling is set to auto
-    autoMode = r.get('autoMode').decode('utf-8')
-
-    if autoMode == '0':
-        while not autoMode:
-            # to avoid excessive querying
-            time.sleep(5)
-            autoMode = r.get('autoMode').decode('utf-8')
-
-    # Get current scheduling jobs from SQL server
-    jobs = ps.getResultSetFromDBNoJS('"Device".view_schedulerinfo',[])
-    print(jobs)
-
-    # store children pids
-    children = []
-
-    # Separate each row into a separate thread
-    for j in jobs:
-        pid = os.fork():
-        if pid == 0:
-            # Store device ID, we run this until day ends
-            deviceID = j['SchedulerDeviceID']
-
-            # Query DB for schedule info
-            schedulerInfo = ps.getResultSetFromDBNoJS('"Device".view_schedulerinfo_single', [deviceID])
-
-            # Query handles time info, run until stop time (query returns nothing)
-            while schedulerInfo:
-                while schedulerInfo['SchedulerTimeUntilStart'] > 0:
-                    # Time remaining is in minutes, convert to seconds and wait
-                    time.sleep(schedulerInfo['SchedulerTimeUntilStart'] * 60)
-                
-                # Send command to redis
-                r.publish('scheduler', json.dumps({
-                'command': schedulerInfo[0]['DeviceType'],
-                'options': {
-                    'key': schedulerInfo[0]['RedisKey'],
-                    'action': 1,
-                    'waitTime': schedulerInfo[0]['SchedulerSecondsToKeepActive']
-                }
-
-                # Sleep for interval
-                time.sleep(schedulerInfo['SchedulerSecondsBetweenActivation'])
-
-                # If end time has passed, kill child
-                if schedulerInfo["SchedulerTimeUntilStop"] < 0:
-                    os._exit(0)
-        else:
-            children.append(pid);
-
-    # parent shouldn't do anything after everything stops
-    while datetime.datetime.now().hour < 20:
-        # If automatic scheduling turned off, kill everything
+    try:
+        # Check if scheduling is set to auto
         autoMode = r.get('autoMode').decode('utf-8')
+
         if autoMode == '0':
+            while not autoMode:
+                # to avoid excessive querying
+                time.sleep(1)
+                autoMode = r.get('autoMode').decode('utf-8')
+
+        # Get current scheduling jobs from SQL server
+        jobs = ps.getResultSetFromDBNoJS('"Device".view_schedulerinfo',[])
+        print(jobs)
+
+        # store children pids
+        children = []
+
+        # Separate each row into a separate thread
+        for j in jobs:
+            pid = os.fork()
+            if pid == 0:
+                # Store device ID, we run this until day ends
+                deviceID = j['SchedulerDeviceID']
+
+                # Query DB for schedule info
+                schedulerInfo = ps.getResultSetFromDBNoJS('"Device".view_schedulerinfo_single', [deviceID])
+
+                # Query handles time info, run until stop time (query returns nothing)
+                while schedulerInfo:
+                    while schedulerInfo[0]['SchedulerTimeUntilStart'] > 0:
+                        # Time remaining is in minutes, convert to seconds and wait
+                        time.sleep(schedulerInfo[0]['SchedulerTimeUntilStart'] * 60)
+
+                    print(f"at: {datetime.datetime.now()}")
+                    print("executing the following:\n" +json.dumps(schedulerInfo,indent=2))
+                    # Send command to redis
+                    r.publish('scheduler', json.dumps({
+                    'command': schedulerInfo[0]['DeviceType'],
+                    'options': {
+                        'key': schedulerInfo[0]['RedisKey'],
+                        'action': 1,
+                        'waitTime': schedulerInfo[0]['SchedulerSecondsToKeepActive']
+                    }
+                    }))
+
+                    # Sleep for interval or wait for sensor value
+                    time.sleep(schedulerInfo[0]['SchedulerSecondsBetweenActivation'])
+
+                # exit process once end time has passed
+                os._exit(0);
+
+            else:
+                children.append(pid);
+
+        # parent shouldn't do anything after everything stops
+        while datetime.datetime.now().minute < 20:
+            # If automatic scheduling turned off, kill everything
+            time.sleep(1)
+            autoMode = r.get('autoMode').decode('utf-8')
+            if autoMode == '0':
+                for child in children:
+                    try:
+                        print(f"at: {datetime.datetime.now()}")
+                        print("killing: " + child)
+                        os.kill(child, 9)
+                    except:
+                        print(f"failed to kill child: {child}")
+
+                break
+
+        
+        # end everything at 8:00 PM
+        if autoMode != 0:
             for child in children:
                 try:
+                    print(f"at: {datetime.datetime.now()}")
+                    print("killing: " + child)
                     os.kill(child, 9)
                 except:
-                    pass
+                    print(f"failed to kill child: {child}")
 
-            break
+            # sleep until 4:00 AM
+            print(f"at: {datetime.datetime.now()}")
+            print(f"sleeping for: {dtu.secondsBeforeTime(4)} seconds")
+            time.sleep(dtu.secondsBeforeTime(4))
 
-    
-    # end everything at 8:00 PM
-    if autoMode != 0:
-        for child in children:
-            try:
-                os.kill(child, 9)
-            except:
-                pass
+        except KeyboardInterrupt:
+            print('Keyboard Interrupt... Shutting down.')
 
-        # sleep until 4:00 AM
-        sleep(dtu.secondsBeforeTime(4))
+            for child in children:
+                try:
+                    print(f"at: {datetime.datetime.now()}")
+                    print("killing: " + child)
+                    os.kill(child, 9)
+                except:
+                    print(f"failed to kill child: {child}")
+            sys.exit()
 
 
                 
